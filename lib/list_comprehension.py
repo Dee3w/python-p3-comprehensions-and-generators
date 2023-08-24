6dc5e0a3f1f681055682712f791c2bb132adf66b
Dee3w <joydanah17@gmail.com>
2023-08-24 13:36:18 +0300

    assignment done

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+      my_list = [n for n in num_list if n%2 == 0]
+      return my_list
 
 def make_exclamation(sentence_list):
-    pass
+      my_list = [n + '!' for n in sentence_list]
+      return my_list 
